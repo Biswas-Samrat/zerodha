e3555cfaf33b15ebe5527c2e5020853245550895
Samrat Biswas <samrat.tx@gmail.com>
2025-05-22 23:02:37 +0600

    Update Signup.js

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -43,7 +43,7 @@ const Signup = () => {
       if (res.data.success) {
         setMessage({ type: "success", text: res.data.message || "Signup successful! Redirecting to dashboard..." });
      
-        window.location.href = "https://zerodha-1-e2f2.onrender.com/dashboard";
+        window.location.href = "https://zerodha-1-e2f2.onrender.com/";
       } else {
        
         setMessage({ type: "danger", text: res.data.message || "Signup failed. Please try again." });
