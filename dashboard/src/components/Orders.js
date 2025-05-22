50d7a2078d21b61fc4238eb3fb526744300290e9
Samrat Biswas <samrat.tx@gmail.com>
2025-05-22 21:32:35 +0600

    Add files via upload

---
+++ dashboard/src/components/Orders.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Order = () => {
+  const [allOrder, setAllOrder] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allOrder").then((res) => {
+      console.log(res.data);
+      setAllOrder(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h3 className="title">Holdings ({allOrder.length})</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Qty.</th>
+            <th>Price</th>
+          </tr>
+
+          {allOrder.map((stock, index) => (
+            <tr key={index}>
+              <td>{stock.name}</td>
+              <td>{stock.qty}</td>
+              <td>{stock.price}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Order;
+
+
+// const Orders = () => {
+//   return (
+//     <div className="orders">
+//       <div className="no-orders">
+//         <p>You haven't placed any orders today</p>
+
+//         <Link to={"/"} className="btn">
+//           Get started
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
